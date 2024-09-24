cbdf73d61349f5803ad96807906c4bfa799680b6
Rafael <rafael_gabriel623@hotmail.com>
2024-09-24 15:30:51 -0300

    Adição do requirements, conexão com banco localmente e adição tag swagger no viewset

--- api_medicamento_anvisa/apps/api/medicamento/viewsets.py
+++ api_medicamento_anvisa/apps/api/medicamento/viewsets.py
@@ -1,20 +1,35 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.exceptions import APIException
 from api_medicamento_anvisa.apps.core.medicamento import models
 from api_medicamento_anvisa.apps.api.medicamento import serializers
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class MedicacaoPagination(PageNumberPagination):
     page_size = 25
 
 
-class MedicacaoViewSet(viewsets.ModelViewSet):
+class MedicacaoViewSet(viewsets.ReadOnlyModelViewSet):  # Mantenha como ReadOnlyModelViewSet
     queryset = models.Medicacao.objects.all()
     serializer_class = serializers.MedicacaoSerializer
     pagination_class = MedicacaoPagination
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'substancia', openapi.IN_QUERY, description="Filtra por substância", type=openapi.TYPE_STRING
+            ),
+            openapi.Parameter(
+                'cnpj', openapi.IN_QUERY, description="Filtra por CNPJ", type=openapi.TYPE_STRING
+            ),
+            openapi.Parameter(
+                'laboratorio', openapi.IN_QUERY, description="Filtra por laboratório", type=openapi.TYPE_STRING
+            ),
+        ]
+    )
     def list(self, request):
         try:
             queryset = self.queryset
@@ -22,9 +37,10 @@ class MedicacaoViewSet(viewsets.ModelViewSet):
                 queryset = self.aplicar_filtros(queryset, request.query_params)
             page = self.paginate_queryset(queryset)
             serializer = self.serializer_class(page, many=True)
-            return Response(serializer.data)
+            return self.get_paginated_response(serializer.data)
         except Exception as exception:
             raise APIException(f"Erro ao listar medicamentos: {str(exception)}")
+
 
     def aplicar_filtros(self, queryset, params):
         substancia = params.get('substancia')
